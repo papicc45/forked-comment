fa76b90343103894fd8aa3f9d626309acfc917b4
papicc45 <papicc45@naver.com>
2023-11-22 09:45:58 +0900

    fix : imported feignClient remove

--- src/main/java/com/weatherfit/comment_service/CommentServiceApplication.java
+++ src/main/java/com/weatherfit/comment_service/CommentServiceApplication.java
@@ -2,7 +2,6 @@ package com.weatherfit.comment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class CommentServiceApplication {
