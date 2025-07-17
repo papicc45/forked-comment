ee130335002d129faa330c8ff6ee3633088d819d
papicc45 <papicc45@naver.com>
2025-07-17 21:29:00 +0900

    2025.07.17
    아이디찾기 성공 테스트 코드 작성 완료
    아이디찾기 인증 과정 실패 테스트 코드 작성 예정

--- src/test/java/com/weatherfit/comment_service/auth/AuthServiceImplTest.java
+++ src/test/java/com/weatherfit/comment_service/auth/AuthServiceImplTest.java
@@ -16,77 +16,82 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.data.redis.core.ReactiveValueOperations;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.config.annotation.web.configurers.UrlAuthorizationConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.util.ReflectionTestUtils;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
-/** ExtendWith - Junit5에 Mockito 기능 통합, @Mock과 @InjectMocks 활성화*/
 @ExtendWith(MockitoExtension.class)
 public class AuthServiceImplTest {
 
-    @Mock
-    UserRepository userRepository;
+    @Mock private UserRepository userRepository;
+    @Mock private PasswordEncoder passwordEncoder;
+    @Mock private JwtTokenProvider jwtTokenProvider;
+    @Mock private UserMapper userMapper;
+    @Mock private ReactiveRedisOperations<String,String> reactiveRedisOperations;
+    @Mock private ReactiveValueOperations<String,String> valueOperations;
+    @Mock private ReactiveHashOperations<String,String,String> hashOperations;
+    @InjectMocks private AuthServiceImpl authService;
 
-    @Mock
-    PasswordEncoder passwordEncoder;
-
-    @Mock
-    JwtTokenProvider jwtTokenProvider;
-
-    @Mock
-    ReactiveStringRedisTemplate redisOperations;
-
-    @Mock
-    private UserMapper userMapper;
-
-    @Mock
-    ReactiveValueOperations<String, String> valueOperations;
-
-    private static final String REDIS_KEY_TOKEN = "email:sign:";
-
-    /** 이 객체 생성할 때, 위 목 객체들 주입*/
-    @InjectMocks
-    AuthServiceImpl authService;
+    private static final String REDIS_KEY_FIND_ID = "FIND_ID_CODE:";
 
     @BeforeEach
     void setUp() {
-        given(redisOperations.opsForValue()).willReturn(valueOperations);
+        lenient().when(reactiveRedisOperations.opsForValue())
+                .thenReturn(valueOperations);
+        lenient().when(reactiveRedisOperations.<String, String>opsForHash())
+                .thenReturn(hashOperations);
     }
-
     @Test
     void signup_success() {
-        UserRequestDTO dto = new UserRequestDTO("alice@example.com", "password", "nick", "emailaa", "01022222222", "token456");
+        UserRequestDTO dto = new UserRequestDTO(
+                "alice@example.com", "password", "dongjun",
+                "nick", "emailaa", "01022222222", "token456"
+        );
+
         User user = new User();
         user.setUserId(dto.getUserId());
         user.setEmail(dto.getEmail());
         user.setPassword("raw");
-
-        LocalDateTime now  = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now();
         ReflectionTestUtils.setField(user, "createdDate", now);
         ReflectionTestUtils.setField(user, "modifiedDate", now);
 
-        given(valueOperations.get("token456")).willReturn(Mono.just("true"));
-        given(userMapper.DTOToUser(dto)).willReturn(user);
-        given(passwordEncoder.encode("password")).willReturn("encodedPwd");
-
+        when(valueOperations.get("token456"))
+                .thenReturn(Mono.just("true"));
+        when(userMapper.DTOToUser(dto))
+                .thenReturn(user);
+        when(passwordEncoder.encode("password"))
+                .thenReturn("encodedPwd");
         ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
-        given(userRepository.save(captor.capture()))
-                .willAnswer(inv -> Mono.just(captor.getValue()));
+        when(userRepository.save(captor.capture()))
+                .thenAnswer(inv -> Mono.just(inv.getArgument(0)));
+        when(valueOperations.delete(anyString()))
+                .thenReturn(Mono.just(true));
 
-        given(redisOperations.opsForValue()).willReturn(valueOperations);
-        given(valueOperations.delete(anyString())).willReturn(Mono.just(true));
-
-        UserResponseDTO responseDTO = new UserResponseDTO(user.getId(), user.getUserId(), user.getNickname(), user.getCreatedDate().toString(), user.getModifiedDate().toString());
-        given(userMapper.userToDTO(user)).willReturn(responseDTO);
+        UserResponseDTO responseDTO = new UserResponseDTO(
+                user.getId(),
+                user.getUserId(),
+                user.getName(),
+                user.getNickname(),
+                user.getCreatedDate().toString(),
+                user.getModifiedDate().toString()
+        );
+        when(userMapper.userToDTO(user))
+                .thenReturn(responseDTO);
 
         StepVerifier.create(authService.signup(Mono.just(dto)))
                 .assertNext(resp -> {
@@ -96,82 +101,130 @@ public class AuthServiceImplTest {
                 })
                 .verifyComplete();
     }
+
     @Test
     void signup_authNumberNotMatch() {
-        UserRequestDTO dto = new UserRequestDTO("alice@example.com", "password", "nick", "emailaa", "01022222222", "token456");
-        given(valueOperations.get("token456")).willReturn(Mono.just("false"));
+        UserRequestDTO dto = new UserRequestDTO(
+                "alice@example.com", "password", "dongjun",
+                "nick", "emailaa", "01022222222", "token456"
+        );
+        when(valueOperations.get("token456"))
+                .thenReturn(Mono.just("false"));
 
         StepVerifier.create(authService.signup(Mono.just(dto)))
-                .expectErrorMatches(ex -> ((BusinessException) ex).getErrorCode() == ErrorCode.AUTH_NUMBER_NOT_MATCH)
+                .expectErrorMatches(ex ->
+                        ((BusinessException) ex).getErrorCode() == ErrorCode.AUTH_NUMBER_NOT_MATCH
+                )
                 .verify();
     }
 
     @Test
     void signup_expiredToken() {
-        UserRequestDTO dto = new UserRequestDTO("alice@example.com", "password", "nick", "emailaa", "01022222222", "token123");
-        given(valueOperations.get("token123")).willReturn(Mono.empty());
+        UserRequestDTO dto = new UserRequestDTO(
+                "alice@example.com", "password", "dongjun",
+                "nick", "emailaa", "01022222222", "token123"
+        );
+        when(valueOperations.get("token123"))
+                .thenReturn(Mono.empty());
 
         StepVerifier.create(authService.signup(Mono.just(dto)))
-                .expectErrorMatches(ex -> ((BusinessException) ex).getErrorCode() == ErrorCode.EXPIRED_TOKEN)
+                .expectErrorMatches(ex ->
+                        ((BusinessException) ex).getErrorCode() == ErrorCode.EXPIRED_TOKEN
+                )
                 .verify();
     }
+
     @Test
     void changePassword_userNotFound() {
+        when(userRepository.findByUserId("alice"))
+                .thenReturn(Mono.empty());
 
-        given(userRepository.findByUserId("alice"))
-                .willReturn(Mono.empty());
-        /** StepVerifier -> Mono나 Flux에 대해 시퀀스 정의, 기대값 검증 */
         StepVerifier.create(authService.changePassword("alice", "old", "new"))
-                .expectErrorMatches(ex -> ex instanceof BusinessException && ((BusinessException) ex).getErrorCode() == ErrorCode.USER_NOT_FOUND)
+                .expectErrorMatches(ex ->
+                        ex instanceof BusinessException
+                                && ((BusinessException) ex).getErrorCode() == ErrorCode.USER_NOT_FOUND
+                )
                 .verify();
     }
 
     @Test
     void changePassword_passwordNotMatch() {
-
         User user = new User();
         user.setUserId("bob");
         user.setPassword("encodedOld");
 
-        given(userRepository.findByUserId("bob"))
-                .willReturn(Mono.just(user));
-
-        given(passwordEncoder.matches("wrongOld", "encodedOld"))
-                .willReturn(false);
+        when(userRepository.findByUserId("bob"))
+                .thenReturn(Mono.just(user));
+        when(passwordEncoder.matches("wrongOld", "encodedOld"))
+                .thenReturn(false);
 
         StepVerifier.create(authService.changePassword("bob", "wrongOld", "newPwd"))
-                .expectErrorMatches(ex -> ((BusinessException) ex).getErrorCode() == ErrorCode.PASSWORD_NOT_MATCH)
+                .expectErrorMatches(ex ->
+                        ((BusinessException) ex).getErrorCode() == ErrorCode.PASSWORD_NOT_MATCH
+                )
                 .verify();
     }
 
     @Test
-    void changePassword_Success() {
+    void changePassword_success() {
         User user = new User();
         user.setUserId("bob");
         user.setPassword("encodedOld");
         user.setPasswordChangedAt(LocalDateTime.now().minusYears(1));
 
-        given(userRepository.findByUserId("bob"))
-                .willReturn(Mono.just(user));
-        given(passwordEncoder.matches("oldPwd", "encodedOld"))
-                .willReturn(true);
-        given(passwordEncoder.encode("newPwd"))
-                .willReturn("encodedNew");
+        when(userRepository.findByUserId("bob"))
+                .thenReturn(Mono.just(user));
+        when(passwordEncoder.matches("oldPwd", "encodedOld"))
+                .thenReturn(true);
+        when(passwordEncoder.encode("newPwd"))
+                .thenReturn("encodedNew");
 
-        /** ArgumentCaptor -> 목 객체 호출시 전달된 파라미터를 캡처해서 나중에 검증하는 역할*/
         ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
-        given(userRepository.save(captor.capture())) /** capture() -> save() 호출 시 넘어온 User 인스턴스를 captor 객체에 저장*/
-                .willAnswer(inv -> Mono.just(inv.getArgument(0))); /** 첫번째 파라미터 꺼내옴*/
-        given(jwtTokenProvider.createToken("bob"))
-                .willReturn("testJwtToken");
+        when(userRepository.save(captor.capture()))
+                .thenAnswer(inv -> Mono.just(inv.getArgument(0)));
+        when(jwtTokenProvider.createToken("bob"))
+                .thenReturn("testJwtToken");
 
         StepVerifier.create(authService.changePassword("bob", "oldPwd", "newPwd"))
                 .assertNext(dto -> {
                     assert dto.getToken().equals("testJwtToken");
                     User saved = captor.getValue();
                     assert saved.getPassword().equals("encodedNew");
-                    assert saved.getPasswordChangedAt().isAfter(LocalDateTime.now().minusSeconds(1));
+                    assert saved.getPasswordChangedAt()
+                            .isAfter(LocalDateTime.now().minusSeconds(1));
                 })
                 .verifyComplete();
+    }
+
+    @Test
+    void findUserId_success() {
+        String email = "alice@example.com";
+        String code = "12345";
+        String userId = "alice123";
+        String key = REDIS_KEY_FIND_ID + email;
+
+        when(hashOperations.entries(key))
+                .thenReturn(Flux.just(Map.entry("code", code), Map.entry("userId", userId)));
+
+        StepVerifier.create(authService.verifyFindUserIdCode(email, code))
+                .expectNext(userId)
+                .verifyComplete();
+    }
+
+    @Test
+    void findUserId_fail() {
+        String email = "alice@example.com";
+        String code = "12345";
+        String userId = "alice123";
+        String key = REDIS_KEY_FIND_ID + email;
+
+        when(hashOperations.entries(key))
+                .thenReturn(Flux.just(Map.entry("code", code), Map.entry("userId", userId)));
+
+        StepVerifier.create(authService.verifyFindUserIdCode(email, "45678"))
+                .expectErrorMatches(ex ->
+                        ((BusinessException) ex).getErrorCode() == ErrorCode.AUTH_NUMBER_NOT_MATCH
+                )
+                .verify();
     }
 }
---
+++ src/test/java/com/weatherfit/comment_service/user/UserServiceImplTest.java
@@ -0,0 +1,83 @@
+package com.weatherfit.comment_service.user;
+
+import com.weatherfit.comment_service.auth.service.AuthServiceImpl;
+import com.weatherfit.comment_service.user.entity.User;
+import com.weatherfit.comment_service.user.repository.UserRepository;
+import com.weatherfit.comment_service.user.service.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import org.springframework.data.redis.core.convert.GeoIndexedPropertyValue;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+    @Mock private UserRepository userRepository;
+    @Mock private AuthServiceImpl authService;
+    @Mock private ReactiveRedisOperations<String,String> reactiveRedisOperations;
+    @Mock private ReactiveValueOperations<String,String> valueOperations;
+    @Mock private ReactiveHashOperations<String,String,String> hashOperations;
+    @InjectMocks private UserServiceImpl userService;
+
+    private static final String REDIS_KEY_FIND_ID = "FIND_ID_CODE:";
+
+    @BeforeEach
+    void setUp() {
+        when(reactiveRedisOperations.<String, String>opsForHash())
+                .thenReturn(hashOperations);
+    }
+
+    @Test
+    void 아이디찾기_success() {
+        User user = new User();
+        user.setName("이동준");
+        user.setEmail("papicc45@naver.com");
+        user.setUserId("papicc45");
+
+        when(userRepository.findByNicknameAndEmail("이동준", "papicc45@naver.com"))
+                .thenReturn(Mono.just(user));
+
+        when(authService.generateRandomCode(15))
+                .thenReturn("123456789012345");
+
+        when(hashOperations.putAll(
+                eq(REDIS_KEY_FIND_ID + user.getEmail()),
+                eq(Map.of("code", "123456789012345", "userId", user.getUserId())))
+        ).thenReturn(Mono.just(true));
+
+        when(reactiveRedisOperations.expire(
+                eq(REDIS_KEY_FIND_ID + user.getEmail()),
+                eq(Duration.ofMinutes(5))
+        )).thenReturn(Mono.just(true));
+
+        when(authService.sendCodeEmail(user.getEmail(), "123456789012345", 2))
+                .thenReturn(Mono.empty());
+
+        StepVerifier.create(userService.checkUserNameAndEmailMatch("이동준", "papicc45@naver.com"))
+                .verifyComplete();
+
+        /** Inorder - 호출 순서 검증 */
+        InOrder order = inOrder(userRepository, authService, hashOperations, reactiveRedisOperations);
+        order.verify(userRepository).findByNicknameAndEmail(user.getName(), user.getEmail());
+        order.verify(authService).generateRandomCode(15);
+        order.verify(hashOperations).putAll(eq(REDIS_KEY_FIND_ID + user.getEmail()), eq(Map.of("code", "123456789012345", "userId", user.getUserId())));
+        order.verify(reactiveRedisOperations).expire(eq(REDIS_KEY_FIND_ID + user.getEmail()), eq(Duration.ofMinutes(5)));
+        order.verify(authService).sendCodeEmail(user.getEmail(), "123456789012345", 2);
+
+    }
+}
